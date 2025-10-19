01904b632e0a6b74ae60251b2f2b8986457adf79
SPandey-87757 <cpandeya@students.federation.edu.au>
2025-10-19 12:24:20 +1100

    ADD EMR SIMULATION WEB APP CONTENTS

---
+++ EMRSimulationWebApp/EMRSimulationWebApp/Models/FoodIntakeAddViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace EMRSimulationWebApp.Models
+{
+    public class FoodIntakeAddViewModel
+    {
+        public int LabId { get; set; }
+        public int PatientId { get; set; }
+
+        public string? Day { get; set; }
+        public DateTime? Date { get; set; }
+
+        public string? Shift1Signature { get; set; }
+        public string? Shift1Designation { get; set; }
+        public string? Shift2Signature { get; set; }
+        public string? Shift2Designation { get; set; }
+    }
+}
